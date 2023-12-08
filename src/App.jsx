1c4e7c4fd96348334716a08263d95186b0880e2a
Maryan Smerchinkiy <maryan.smerchinskiy@gmail.com>
2023-12-08 18:40:09 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import Header from './Components/header/Index';
 import Burgers from './Components/menuContainer/Burgers/Burgers';
 import Pizza from './Components/menuContainer/Pizza/Pizza';
@@ -11,30 +11,54 @@ import Kebab from './Components/menuContainer/Kebab/Kebab';
 import Snack from './Components/menuContainer/Snack/Snack';
 import HotDog from './Components/menuContainer/HotDog/HotDog';
 import Sets from './Components/menuContainer/Sets/Sets';
+import { createBrowserHistory } from 'history';
 
-
-
-
+const history = createBrowserHistory();
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route path="/" element={<Header/>}>
-            <Route index element={<HomePage/>} />
-            <Route path="Pizza" element={<Pizza/>} />
-            <Route path="Burgers" element={<Burgers/>} />
-            <Route path="Kebab" element={<Kebab/>} />
-            <Route path="Snack" element={<Snack/>} />
-            <Route path="HotDog" element={<HotDog/>} />
-            <Route path="Sets" element={<Sets/>} />
-            <Route path="Drinks" element={<Drinks/>} />
-            <Route path="Appendices" element={<Appendices/>} />
-            <Route path="Bucket" element={<Bucket/>} />
-  </Route> 
+  <Route path="/" element={<Header />}>
+    <Route index element={<HomePage />} />
+    <Route path="Pizza" element={<Pizza />} />
+    <Route path="Burgers" element={<Burgers />} />
+    <Route path="Kebab" element={<Kebab />} />
+    <Route path="Snack" element={<Snack />} />
+    <Route path="HotDog" element={<HotDog />} />
+    <Route path="Sets" element={<Sets />} />
+    <Route path="Drinks" element={<Drinks />} />
+    <Route path="Appendices" element={<Appendices />} />
+    <Route path="Bucket" element={<Bucket />} />
+  </Route>
+), { history });
 
-))
 function App() {
   return (
-  <>
-    <RouterProvider router={router}/>
-  </>
+    <RouterProvider router={router}>
+      {/* Your entire application goes here */}
+      {/* For example, you might want to include a layout component */}
+      <div>
+        <Route path="/" element={<Header />} />
+        {/* Add a container for your main content */}
+        <div>
+          {/* Include the route outlet where your components will be rendered */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Route index element={<HomePage />} />
+                <Route path="Pizza" element={<Pizza />} />
+                <Route path="Burgers" element={<Burgers />} />
+                <Route path="Kebab" element={<Kebab />} />
+                <Route path="Snack" element={<Snack />} />
+                <Route path="HotDog" element={<HotDog />} />
+                <Route path="Sets" element={<Sets />} />
+                <Route path="Drinks" element={<Drinks />} />
+                <Route path="Appendices" element={<Appendices />} />
+                <Route path="Bucket" element={<Bucket />} />
+              </>
+            }
+          />
+        </div>
+      </div>
+    </RouterProvider>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import store from './Redux/Store.jsx'
-import { Provider } from 'react-redux'
-import { createRoot } from 'react-dom';
+
+import { Provider } from 'react-redux';
 
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
     <Provider store={store}>
       <App />
     </Provider>
-
 )
